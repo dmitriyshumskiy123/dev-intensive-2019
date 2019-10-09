4467a1e755ee2a9e867c06d3a7e788d3dd90c816
Dmitriy <dmitriyshumskiy123@yandex.ru>
2019-10-09 16:52:02 +0300

    cutting the string

--- app/src/main/java/ru/skillbranch/devintensive/Extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/Extensions/String.kt
@@ -9,3 +9,14 @@ import java.util.*
 /*private fun Date.convertFromDuration(): Any {
 
 }*/
+
+fun String.truncate(str: String, num: Int = 16) : String
+{
+    if (str.length >= num) return str
+    else {
+        var str1 = str.substring(0, num-1)
+        if (str1[num-1] == ' ') str1 = str1.substring(0, num-2)
+        str1 += "..."
+        return str1
+    }
+}
